db8dea2d9e606a715bffeacdda2b7ca50072f550
U200709050 <emirhanpakyardim@gmail.com>
2023-01-08 09:40:08 +0300

    feat(loading): add loading screen while fetching data

--- src/components/Initial.js
+++ src/components/Initial.js
@@ -1,76 +1,85 @@
-import React from 'react';
-import QuizScreen from './QuizScreen';
+import React, { useState, useEffect } from "react";
+import QuizScreen from "./QuizScreen";
+import Loading from "./Loading";
 
-const Initial = () => {
+const Initial = (props) => {
+  const [category, setCategory] = useState({ value: "any" });
+  const [loading, setLoading] = useState(false);
+  const [isFetched, setIsFetched] = useState(false);
+  const [allQuestions, setAllQuestions] = useState([]);
 
-    const [category, setCategory] = React.useState({value: "any"});
-    const [allQuestions, setAllQuestions] = React.useState([]);
-    
+  const handleChange = (e) => {
+    setCategory({ value: e.target.value });
+  };
 
-    const [start, setStart] = React.useState(false);
+  useEffect(() => {
+    console.log("loading", loading);
+    console.log(allQuestions);
+  }, [loading, allQuestions]);
 
-    const handleChange = (e) => {
-        setCategory({value: e.target.value});
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    const toggleStart = () => {
-        setStart(!start);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        toggleStart();
-
-        fetch(`https://opentdb.com/api.php?amount=5&${category.value === 'any' ? "" : `category=${category.value}&`}type=multiple`)
-            .then(res => res.json())
-            .then(question => setAllQuestions(question.results));
-
-    }
-
-
-    return (
-        <div>
-        {!start && <div className='startDiv'>
-            <h1 className="title">Necmo Trivia</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Pick a category and start!
-                <select value={category.value} onChange={handleChange} className='selectCategory'>
-                    <option value="any">Any Category</option>
-                    <option value="9">General Knowledge</option>
-                    <option value="10">Books</option>
-                    <option value="11">Film</option>
-                    <option value="12">Music</option>
-                    <option value="13">Musicals & Theatres</option>
-                    <option value="14">Television</option>
-                    <option value="15">Video Games</option>
-                    <option value="16">Board Games</option>
-                    <option value="17">Science & Nature</option>
-                    <option value="18">Computers</option>
-                    <option value="19">Mathematics</option>
-                    <option value="20">Mythology</option>
-                    <option value="21">Sports</option>
-                    <option value="22">Geography</option>
-                    <option value="23">History</option>
-                    <option value="24">Politics</option>
-                    <option value="25">Art</option>
-                    <option value="26">Celebrities</option>
-                    <option value="27">Animals</option>
-                    <option value="28">Vehicles</option>
-                    <option value="29">Comics</option>
-                    <option value="30">Gadgets</option>
-                    <option value="31">Anime & Manga</option>
-                    <option value="32">Cartoon & Animations</option>
-                </select>
-                </label>
-
-                <button className='startBtn'>Start Necmo Quiz</button>
-            </form>
-
-            </div>}
-
-            {start && <QuizScreen questions={allQuestions}/>}
-        </div>
+    fetch(
+      `https://opentdb.com/api.php?amount=5&${
+        category.value === "any" ? "" : `category=${category.value}&`
+      }type=multiple`
     )
-}
+      .then((res) => res.json())
+      .then((question) => setAllQuestions(question.results))
+      .then(() => setLoading(false))
+      .then(() => setIsFetched(true))
+  };
+
+  return (
+    <div>
+      {loading ? <Loading/> : !isFetched ? (
+        <div className="startDiv">
+          <h1 className="title">Necmo Trivia</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Pick a category and start!
+              <select
+                value={category.value}
+                onChange={handleChange}
+                className="selectCategory"
+              >
+                <option value="any">Any Category</option>
+                <option value="9">General Knowledge</option>
+                <option value="10">Books</option>
+                <option value="11">Film</option>
+                <option value="12">Music</option>
+                <option value="13">Musicals & Theatres</option>
+                <option value="14">Television</option>
+                <option value="15">Video Games</option>
+                <option value="16">Board Games</option>
+                <option value="17">Science & Nature</option>
+                <option value="18">Computers</option>
+                <option value="19">Mathematics</option>
+                <option value="20">Mythology</option>
+                <option value="21">Sports</option>
+                <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="25">Art</option>
+                <option value="26">Celebrities</option>
+                <option value="27">Animals</option>
+                <option value="28">Vehicles</option>
+                <option value="29">Comics</option>
+                <option value="30">Gadgets</option>
+                <option value="31">Anime & Manga</option>
+                <option value="32">Cartoon & Animations</option>
+              </select>
+            </label>
+            <button className="startBtn">Start Necmo Quiz</button>
+          </form>
+        </div>
+      ) : (
+        <QuizScreen />
+      )}
+    </div>
+  );
+};
 
 export default Initial;
---
+++ src/components/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div class="loader">
+      <div class="loader-inner">
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+        <div class="loader-line-wrap">
+          <div class="loader-line"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
